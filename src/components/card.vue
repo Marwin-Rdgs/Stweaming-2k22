e93e44feebaa2f9a569be3d470e3616ab07645e2
Marwin <80853859+Marwin-Rdgs@users.noreply.github.com>
2022-06-26 14:57:47 +0200

    Modification card (like => Ajout & modification font)

--- src/components/card.vue
+++ src/components/card.vue
@@ -9,18 +9,18 @@
 
     <!-- Textual -->
     <div class="ml-2">
-    <h2 class="text-xl">Your Name</h2>
-    <p>Biographie de la série, film, dessin animé, anime, ...</p>
+    <h2 class="text-xl font-kyivtype-sans">Your Name</h2>
+    <p class="font-montserrat">Biographie de la série, film, dessin animé, anime, ...</p>
     </div>
 
     <!-- Fav & Visionner -->
     <div class="flex justify-between mt-4 lg:mr-4 lg:ml-4 pb-8">
-        <button class="border-2 border-noir p-1 rounded-2xl hover:bg-noir hover:text-blanc-b">
+        <button class="border-2 border-noir p-1 rounded-2xl hover:bg-noir hover:text-blanc-b font-montserrat">
             Visionner
         </button>
 
-        <button class="border-2 border-noir rounded-full py-1 px-3">
-            <p class="text-lg">♡</p>
+        <button class="border-2 border-noir rounded-full px-3 hover:bg-noir hover:text-blanc-b">
+            <p class="text-lg">+</p>
         </button>
     </div>
 
